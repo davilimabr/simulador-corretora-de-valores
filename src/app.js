85b2813d4091a211de08a783b7ce020a4d7361ed
davilimabr <davilimamoura.360@gmail.com>
2025-07-07 19:42:00 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -2,15 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
+import cors from 'cors';
 import routes from './routes/index.js';
 import './config/database.js';
-import { sequelize } from './models/index.js';
+import sequelize from './models/index.js';
 
 dotenv.config();
 
 const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+}));
 
 // Rotas
 app.use('/api', routes);
